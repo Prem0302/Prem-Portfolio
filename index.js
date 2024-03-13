d75c93feb35dec3c8e76e6ae6b60e37158087fd2
Prem0302 <prembca28@gmail.com>
2024-03-13 22:18:04 +0530

    hosting project

---
+++ index.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_eekld7l";
+    const templateID = "template_y04gmjc";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  }
